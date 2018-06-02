c23dd1e6d71a1af48f5db7ab453909e4871f6c8f
MiLaN <milan.pokharna1998@gmail.com>
2018-06-02 18:04:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/lenovo/button3d/Main12Activity.java
@@ -0,0 +1,175 @@
+package com.example.lenovo.button3d;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class Main12Activity extends AppCompatActivity implements  View.OnClickListener{
+//level - 11(level-16)
+static int goal = 24;
+    static int  result = 0;
+    static int moves = 6 ;
+    static int count11 = 10;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main12);
+        Button clear=(Button)findViewById(R.id.bclear);
+        ImageButton setting =(ImageButton)findViewById(R.id.setting);
+        Button black1= (Button)findViewById(R.id.black1);
+        Button black2= (Button)findViewById(R.id.black2);
+        Button black3= (Button)findViewById(R.id.black3);
+        Button b1= (Button)findViewById(R.id.b1);
+        Button b2= (Button)findViewById(R.id.b2);
+        Button b3= (Button)findViewById(R.id.b3);
+        Button b4=(Button)findViewById(R.id.b4);
+        TextView Result=(TextView)findViewById(R.id.answer);
+        TextView Goal=(TextView)findViewById(R.id.goal);
+        TextView Level=(TextView)findViewById(R.id.level);
+        TextView move = (TextView)findViewById(R.id.moves);
+        black1.setOnClickListener(this);
+        black2.setOnClickListener(this);
+        black3.setOnClickListener(this);
+        b2.setOnClickListener( this );
+        black1.setText("+5");
+        Result.setText(""+result);
+        black2.setText("x3");
+        black3.setText("<<");
+        Goal.setText("GOAL : 24");
+        Level.setText("LEVEL 11");
+        b1.setEnabled(false);
+        b2.setText( "x5" );
+        b3.setEnabled(false);
+        move.setText("MOVES :"+moves);
+        Result.setText(""+result);
+        b4.setEnabled(false);
+        TextView myTextView = (TextView) findViewById(R.id.answer);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/digital.ttf");
+        myTextView.setTypeface(typeface);
+        //black3.setEnabled(false);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (moves > 0) {
+            Button clear=(Button)findViewById(R.id.bclear);
+            TextView Result = (TextView) findViewById(R.id.answer);
+            TextView move = (TextView) findViewById(R.id.moves);
+
+            switch (v.getId()) {
+
+                case R.id.black1:{
+
+                    final MediaPlayer mp = MediaPlayer.create(this, R.raw.bclick);
+                    mp.start();
+                    result = result + 5  ;
+                    Result.setText("" + result);
+                    --moves;
+                    move.setText("MOVES: " + moves);
+
+                    break;}
+                case R.id.black2: {
+
+                    final MediaPlayer mp = MediaPlayer.create(this, R.raw.bclick);
+                    mp.start();
+                    result = result * 3;
+                    Result.setText("" + result);
+                    --moves;
+                    move.setText("MOVES: " + moves);
+                    break;
+                }
+                case R.id.black3: {
+
+                    final MediaPlayer mp = MediaPlayer.create(this, R.raw.bclick);
+                    mp.start();
+                    result = result / 10;
+                    Result.setText("" + result);
+                    --moves;
+                    move.setText("MOVES: " + moves);
+                    break;
+                }
+                case R.id.b2 :{
+                    final MediaPlayer mp = MediaPlayer.create(this, R.raw.bclick);
+                    mp.start();
+                    result = result  * 5 ;
+                    Result.setText("" + result);
+                    --moves;
+                    move.setText("MOVES: " + moves);
+                    break;
+
+                }
+
+
+
+
+            }
+            if(result < 0)
+            {
+                clear();
+            }
+            if (goal == result) {
+                Result.setText("YOU WIN");
+                count11 = count11+10;
+                new Handler().postDelayed( new Runnable() {
+                    @Override
+                    public void run() {
+
+                        Intent i=new Intent(Main12Activity.this,Main13Activity.class);
+                        finish();
+                        startActivity(i);
+                    }
+                }, 450);
+            }
+            else{
+                if(moves==0&&goal!=result)
+                {
+                    Result.setTextSize(65);
+                    Result.setText("YOU LOOSE");
+                }
+            }
+
+        }
+    }
+    public void bclear(View view){
+        clear();
+    }
+    public void clear(){
+        TextView Result = (TextView) findViewById(R.id.answer);
+        TextView move = (TextView) findViewById(R.id.moves);
+
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.bclick);
+        mp.start();
+        count11 = count11-5;
+
+        moves = 6;
+        result = 0;
+        Result.setText("" + result);
+        move.setText("MOVES: " + moves);
+    }
+    public void setClick(View view)
+    {
+
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.bclick);
+        mp.start();
+        Intent i=new Intent(Main12Activity.this,SettingActivity.class);
+        TextView Level=(TextView)findViewById(R.id.level);
+        i.putExtra("level11", count11);
+        i.putExtra("calling-activity", MainActivity.ActivityConstants.ACTIVITY_11);
+        i.putExtra("textViewText", Level.getText().toString());
+        startActivity(i);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/lenovo/button3d/scoreActivity.java
@@ -0,0 +1,76 @@
+package com.example.lenovo.button3d;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tushar on 6/11/17.
+ */
+
+public class scoreActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.score_layout);
+        TextView level1 = (TextView) findViewById(R.id.level1);
+        TextView level2 = (TextView) findViewById(R.id.level2);
+        TextView level3 = (TextView) findViewById(R.id.level3);
+        TextView level4 = (TextView) findViewById(R.id.level4);
+        TextView level5 = (TextView) findViewById(R.id.level5);
+        TextView level6 = (TextView) findViewById(R.id.level6);
+        TextView level7 = (TextView) findViewById(R.id.level7);
+        TextView level8 = (TextView) findViewById(R.id.level8);
+        TextView level9 = (TextView) findViewById(R.id.level9);
+        TextView level10 = (TextView) findViewById(R.id.level10);
+        TextView level11 = (TextView) findViewById(R.id.level11);
+        TextView level12 = (TextView) findViewById(R.id.level12);
+        TextView level13 = (TextView) findViewById(R.id.level13);
+        TextView level14 = (TextView) findViewById(R.id.level14);
+        TextView level15 = (TextView) findViewById(R.id.level15);
+        TextView myTextView = (TextView) findViewById(R.id.lel);
+        TextView src = (TextView) findViewById(R.id.scr);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/digital.ttf");
+        myTextView.setTypeface(typeface);
+        src.setTypeface(typeface);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.bclick);
+        mp.start();
+        List<Integer> list = getIntent().getExtras().getIntegerArrayList("code");
+        level1.setText(""+list.get(0));
+        level2.setText(""+list.get(1));
+        level3.setText(""+list.get(2));
+        level4.setText(""+list.get(3));
+        level5.setText(""+list.get(4));
+        level6.setText(""+list.get(5));
+        level7.setText(""+list.get(6));
+        level8.setText(""+list.get(7));
+        level9.setText( ""+list.get( 8 ) );
+        level10.setText( ""+list.get( 9 ) );
+        level11.setText( ""+list.get( 10 ) );
+        level12.setText( ""+list.get( 11 ) );
+        level13.setText( ""+list.get( 12 ) );
+        level14.setText( ""+list.get( 13 ) );
+        level15.setText( ""+list.get( 14 ) );
+
+
+
+    }
+        public void back(View view)
+    {
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.bclick);
+        mp.start();
+       Intent intent = new Intent(scoreActivity.this,SettingActivity.class);
+       startActivity(intent);
+    }
+}
